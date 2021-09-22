53246892f338165283161ebc1f51a0e5bea1b917
matteusbonotto <matteusbonotto@gmail.com>
2021-09-21 22:33:43 -0300

    ajustes simples
    ajustes simples

--- listarUsuarios.php
+++ listarUsuarios.php
@@ -9,87 +9,84 @@ $msg = "nenhum usuario encontrado";
 <html lang="en">
 
 <head>
-	<title>Cadastro</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <title>Cadastro</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
-<!--===============================================================================================-->
+    <!--===============================================================================================-->
 </head>
 
 <body>
-	
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Usuário</th>
-      <th scope="col">Senha</th>
-    </tr>
-  </thead>
-  <tbody>   
-                    <?php 
 
-                <table class="table">
-                    <thead>
-                        <th> ID </th>
-                        <th> usuario </th>
-                        <th> senha </th>
-                    </thead>
-                    <tbody>
-                        <?php
-
-                        while ($usuario = mysqli_fetch_array($result)) {
-                            echo "
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Usuário</th>
+                <th scope="col">Senha</th>
+            </tr>
+        </thead>
+        <tbody>
+            <table class="table">
+                <thead>
+                    <th> ID </th>
+                    <th> usuario </th>
+                    <th> senha </th>
+                </thead>
+                <tbody>
+                    <?php
+                    while ($usuario = mysqli_fetch_array($result)) {
+                        echo "
                             <tr> 
                             <td>" . $usuario['id'] . "</td>     
                             <td>" . $usuario['usuario'] . "</td>     
                             <td> •••••••••••••••••••••••••</td>
-                            </tr>";     
-                        }
-                        ?>
-                    </tbody>
-                </table>
+                            </tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
 
             </div>
-        </div>
-    </div>
+            </div>
+            </div>
 
 
 
 
-    <!--===============================================================================================-->
-    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-    <!--===============================================================================================-->
-    <script src="vendor/bootstrap/js/popper.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-    <!--===============================================================================================-->
-    <script src="vendor/select2/select2.min.js"></script>
-    <!--===============================================================================================-->
-    <script src="vendor/tilt/tilt.jquery.min.js"></script>
-    <script>
-        $('.js-tilt').tilt({
-            scale: 1.1
-        })
-    </script>
-    <!--===============================================================================================-->
-    <script src="js/main.js"></script>
+            <!--===============================================================================================-->
+            <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+            <!--===============================================================================================-->
+            <script src="vendor/bootstrap/js/popper.js"></script>
+            <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+            <!--===============================================================================================-->
+            <script src="vendor/select2/select2.min.js"></script>
+            <!--===============================================================================================-->
+            <script src="vendor/tilt/tilt.jquery.min.js"></script>
+            <script>
+                $('.js-tilt').tilt({
+                    scale: 1.1
+                })
+            </script>
+            <!--===============================================================================================-->
+            <script src="js/main.js"></script>
 
 </body>
-</html>
 
+</html>
